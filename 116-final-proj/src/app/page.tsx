8110231502d22c62c3f9b3d275668168eb3209bf
Bryan Jiang <bryanjiang04@gmail.com>
2024-12-15 21:46:51 -0800

    title

--- 116-final-proj/src/app/page.tsx
+++ 116-final-proj/src/app/page.tsx
@@ -8,23 +8,31 @@ export default function Home() {
     <div className="grid grid-rows-[auto_1fr_auto] min-h-screen p-4 sm:p-10 gap-6 font-luxury bg-base-100 text-black">
       {/* Header with Title */}
       <header className="row-start-1 text-center">
-        <h1 className="text-4xl sm:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-gray-900 via-gray-500 to-gray-500">[INSERT TITLE]</h1>
-        <p className="text-sm sm:text-md text-gray-400 mt-2 italic tracking-wide">
-          Created by Alex Danilkovas, Bryan Jiang, Jason Saez, Eric Xiao
-        </p>
-      </header>
+      <div className="p-4">
+        <h1 className="text-4xl sm:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-gray-900 via-gray-500 to-gray-500">
+          U.S. Real Estate Insights by County
+        </h1>
+      </div>
+      <p className="text-sm sm:text-md text-gray-400 mt-2 italic tracking-wide">
+        Created by Alex Danilkovas, Bryan Jiang, Jason Saez, Eric Xiao
+      </p>
+    </header>
 
       {/* Main Content */}
       <main className="row-start-2 grid grid-cols-1 md:grid-cols-3 gap-6">
         {/* Left: Map Visualization */}
         <div className="md:col-span-2 bg-white rounded-lg shadow-xl p-4">
-          <h2 className="text-2xl font-light mb-4 text-neutral">Geographical Analysis</h2>
+          <h2 className="text-2xl font-sans font-bold mb-4 text-neutral">
+            Geographical Analysis
+          </h2>
           <div id="map-container" className="w-full h-full"></div>
         </div>
 
         {/* Right: Visualization Placeholder */}
         <div className="bg-white rounded-lg shadow-xl p-4">
-          <h2 className="text-2xl font-light mb-4 text-neutral">Selected Counties</h2>
+          <h2 className="text-2xl font-sans font-bold mb-4 text-neutral">
+            Selected Counties
+          </h2>
           <div id="table-container">
             {/* <CountyTable /> */}
           </div>
@@ -33,7 +41,9 @@ export default function Home() {
 
       {/* Bottom Visualization */}
       <section className="row-start-3 bg-white rounded-lg shadow-xl p-4">
-        <h2 className="text-2xl font-light mb-4 text-neutral">Trends & Comparisons</h2>
+        <h2 className="text-2xl font-sans font-bold mb-4 text-neutral">
+          Trends & Comparisons
+        </h2>
         <div id="barchart-container" className="w-full h-full"></div>
       </section>
 
